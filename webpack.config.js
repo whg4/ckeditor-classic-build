9cd087f464880196d47ebebe5404985e60b5b9a5
liuweihua <liuweihua@stary.com>
2023-06-29 10:24:50 +0800

    feat: Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+// webpack.config.js
+
+'use strict';
+
+const path = require('path');
+const { styles, loaders } = require('@ckeditor/ckeditor5-dev-utils');
+
+module.exports = {
+	devtool: 'source-map',
+
+	// https://webpack.js.org/configuration/entry-context/
+	entry: path.join(__dirname, './src/ckeditor.ts'),
+
+	// https://webpack.js.org/configuration/output/
+	output: {
+		library: 'ClassicEditor',
+
+		path: path.resolve(__dirname, 'build'),
+		filename: 'ckeditor.js',
+		libraryTarget: 'umd',
+		libraryExport: 'default'
+	},
+
+	module: {
+		rules: [
+			{
+				test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+
+				use: ['raw-loader']
+			},
+			{
+				test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							injectType: 'singletonStyleTag',
+							attributes: {
+								'data-cke': true
+							}
+						}
+					},
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: styles.getPostCssConfig({
+								themeImporter: {
+									themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+								},
+								minify: true
+							})
+						}
+					},
+				]
+			},
+			loaders.getTypeScriptLoader()
+		]
+	},
+
+	// Useful for debugging.
+	devtool: 'source-map',
+
+	// By default webpack logs warnings if the bundle is bigger than 200kb.
+	performance: { hints: false }
+};
